3cc39c9cf1b189a790c90231f798635783e9ec0c
houhong <houhong@tydic.com>
2022-08-11 17:37:11 +0800

    --date: 2022-08-11 --fix: 基于springboot-webSocket的消息推送

--- algorithm/src/main/java/com/houhong/algorithm/ribbon/ConstantsHash.java
+++ algorithm/src/main/java/com/houhong/algorithm/ribbon/ConstantsHash.java
@@ -2,7 +2,7 @@ package com.houhong.algorithm.ribbon;
 
 /**
  * @program: algorithm-work
- * @description: 一致性哈希算法
+ * @description: 一致性哈希算法 todo 2022-08-11 今晚的算法题：写一致性轮训算法
  * @author: houhong
  * @create: 2022-08-11 16:22
  **/
---
+++ algorithm/src/main/java/com/houhong/business/WebSocketDemo.java
@@ -0,0 +1,12 @@
+package com.houhong.business;
+
+/**
+ * @program: algorithm-work
+ * @description: websocketDemo 用来实现消息推送
+ * @author: houhong
+ * @create: 2022-08-11 17:09
+ **/
+public class WebSocketDemo {
+
+    
+}
