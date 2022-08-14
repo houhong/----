c2a0ea98d127abb63fd33edc31b0000134029ccd
houhong <houhong@tydic.com>
2022-08-14 15:11:31 +0800

    -- 二叉树层次遍历-- 自底向上输出

--- algorithm/src/main/java/com/houhong/algorithm/tree/LevelTrave.java
+++ algorithm/src/main/java/com/houhong/algorithm/tree/LevelTrave.java
@@ -84,6 +84,44 @@ public class LevelTrave {
 
     }
 
+    /**
+     * 二叉树层次遍历。 自底向上输出结果
+     * 【
+     * <p>
+     * [],
+     * [],
+     * []
+     * <p>
+     * 】
+     **/
+    public List<List<Integer>> levelOrderTravse(TreeNode root) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        levelOrder(root, 0, ans);
+
+        for (int start = 0, end = ans.size() - 1; start < end; start++, end--) {
+
+            swap(start, end, ans);
+
+        }
+        return ans;
+
+    }
+
+    private static void swap(int first, int second, List<List<Integer>> ans) {
+
+        List<Integer> temp = new ArrayList<>();
+
+        List<Integer> fi = ans.get(first);
+        List<Integer> end = ans.get(second);
+        temp = fi;
+        fi = end;
+        end = temp;
+        ans.set(first, fi);
+        ans.set(second, end);
+
+    }
+
 
     public List<List<Integer>> traceLevelForN(Node root, int k, List<List<Integer>> ans) {
 
@@ -100,12 +138,47 @@ public class LevelTrave {
         return ans;
     }
 
-
     public List<List<Integer>> levelOrderForN(Node root) {
 
         List<List<Integer>> ans = new ArrayList<>();
         traceLevelForN(root, 0, ans);
         return ans;
+    }
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        List<Integer> fi = new ArrayList<>();
+        fi.add(0);
+        ans.add(fi);
+
+        List<Integer> se = new ArrayList<>();
+        se.add(0);
+        se.add(1);
+        ans.add(se);
+
+
+        List<Integer> thi = new ArrayList<>();
+        thi.add(0);
+        thi.add(1);
+        thi.add(2);
+        ans.add(thi);
+
+        for (int start = 0, end = ans.size() - 1; start < end; start++, end--) {
+
+            swap(start, end, ans);
+
+        }
+
+        for (List<Integer> an : ans) {
+
+            for (Integer integer : an) {
+                System.out.print(integer);
+            }
+            System.out.println();
+        }
+
     }
 }
 
